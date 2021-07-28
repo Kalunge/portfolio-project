7d69fc817bfb1fd190e810fc9e185b97f6c5c9e3
ali abbani <aliox@ahlam.com>
2021-07-28 15:39:52 +0300

    add form.js file and start the js code

---
+++ form.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form');
+const email = document.getElementById('email');
+const message = document.getElementById('small');
+
+function validateEmail(email){
+    if(email !== email.toLowerCase()) {
+      const showMessage = 'email should be in lower case';
+    }
+    
+}
+
