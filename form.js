227d74ee006201b6b40be528c068dd6c285c6bdf
kalunge <muthomititus+github@gmail.com>
2021-07-28 14:30:09 +0100

    Add logic to prevent submission before validation

--- form.js
+++ form.js
@@ -1,10 +1,9 @@
 const form = document.querySelector('form');
 const email = document.getElementById('email');
-const message = document.getElementById('small');
+const messageField = document.getElementById('small');
 
 function validateEmail(email) {
-  const regEx =
-    /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+  const regEx = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
   if (regEx.test(email)) {
     return true;
   }
@@ -13,8 +12,8 @@ function validateEmail(email) {
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (!validateEmail(email)) {
-    message.innerText = '(*) Please provide a email in lower case';
+  if (!validateEmail(email.value)) {
+    messageField.innerText = `(*) Please provide an email in lower case eg ${email.value.toLocaleLowerCase()}`;
   } else {
     form.submit();
   }
