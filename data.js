4ab8a91d1b5eaa6900466db515c4bebae7da6b95
ali abbani <aliox@ahlam.com>
2021-07-29 15:10:44 +0300

    create a data.js file

---
+++ data.js
@@ -0,0 +1,4 @@
+const form = document.querySelector('form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
